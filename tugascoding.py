a845e0a957fc4532e8f81de7c698d8c8fbcff560
VRWY <113221647+VRWY@users.noreply.github.com>
2022-09-14 17:38:26 +0700

    Add files via upload

---
+++ tugascoding.py
@@ -0,0 +1,26 @@
+print("MENGHITUNG KUBUS DAN BALOK")
+#volume kubus
+print("Menghitung volume kubus")
+s = ("panjang rusuk = 4")
+rumus = ("Rumus volume kubus adalah s*s*s")
+VK = 4*4*4
+volume = ("volume kubus adalah", VK)
+
+#volume balok
+balok = ("Menghitung volume balok")
+p = ("panjang = 8")
+l = ("laus = 6")
+t = ("tinggi = 7")
+volume_balok = ("Rumus volume balok adalah p*l*t")
+VB = 8*6*7
+volume = ("volume balok adalah", VB)
+
+print(s)
+print(rumus)
+print(volume)
+print(balok)
+print(p)
+print(l)
+print(t)
+print(volume_balok)
+print(volume)
